da8958ae8303a83e9c37f31491483b4422c9191c
ehuntober <ehuntercrypto@gmail.com>
2023-07-01 10:08:28 +0000

    new budget update added

--- controllers/budgetController.js
+++ controllers/budgetController.js
@@ -28,7 +28,48 @@ const createBudget = async (req, res) => {
   }
 };
 
+const updateBudget = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, totalAmount } = req.body;
+
+    const updatedBudget = await Budget.findByIdAndUpdate(
+      id,
+      { name, totalAmount },
+      { new: true }
+    );
+
+    if (!updatedBudget) {
+      return res.status(404).json({ error: 'Budget not found' });
+    }
+
+    res.json(updatedBudget);
+  } catch (error) {
+    console.error('Error updating the budget', error);
+    res.status(500).json({ error: 'An error occurred while updating the budget' });
+  }
+};
+
+const deleteBudget = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedBudget = await Budget.findByIdAndDelete(id);
+
+    if (!deletedBudget) {
+      return res.status(404).json({ error: 'Budget not found' });
+    }
+
+    res.json({ message: 'Budget deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting the budget', error);
+    res.status(500).json({ error: 'An error occurred while deleting the budget' });
+  }
+};
 module.exports = {
+
   getAllBudgets,
   createBudget,
+  updateBudget,
+  deleteBudget,
 };
--- routes/budget.js
+++ routes/budget.js
@@ -6,5 +6,8 @@ const budgetController = require('../controllers/budgetController');
 router.get('/', budgetController.getAllBudgets);
 router.post('/', budgetController.createBudget);
 
+router.put('/:id', budgetController.updateBudget);
+router.delete('/:id',budgetController.deleteBudget)
+
 module.exports = router;
 
