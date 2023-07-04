f7a377a5925322973ab41b2c5eddcf184ca20f67
ehuntober <ehuntercrypto@gmail.com>
2023-07-04 11:44:23 +0000

    filtered expenses adde

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -75,6 +75,38 @@ const deleteExpense = async (req, res) => {
   }
 };
 
+const getFilteredAndSortedExpenses = async (req, res) => {
+  try{
+    const {budgetId,category,sortBy,sortOrder} = req.query;
+
+    const filter = {};
+
+    if(budgetId) {
+      filter.budgetId = budgetId;
+    }
+
+    if(category){
+      filter.category = category;
+    }
+
+    const sort = {};
+    if (sortBy && sortOrder) {
+      sort[sortBy] = sortOrder === 'asc' ? 1 : -1;
+    }
+
+    // Fetch the expenses based on the filter and sort options
+    const expenses = await Expense.find(filter).sort(sort);
+
+    res.status(200).json(expenses);
+  } catch (error) {
+    console.error('Error fetching filtered and sorted expenses', error);
+    res.status(500).json({ error: 'An error occurred while fetching expenses' });
+  }
+  }
+
+
+
+
 
 
 
@@ -83,4 +115,5 @@ module.exports = {
   createExpense,
   updateExpense,
   deleteExpense,
+  getFilteredAndSortedExpenses;
 };
